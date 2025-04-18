90da70de2328790e52aa64fc4f0ae9579f3264cd
Harvox <134923684+Harvox@users.noreply.github.com>
2025-04-18 04:11:08 -0400

    Update frappe cafe.lua

--- frappe cafe.lua
+++ frappe cafe.lua
@@ -1,6 +1,7 @@
 --[[ 
 bruh theyre using dev forum env leak detect
 ]]
+function lol() end
 
 local Old; Old = hookfunction(getrenv().getfenv, function(...)
 if tostring(getcallingscript()) == "Controller" then 
@@ -8,3 +9,17 @@ return {}
 end
 return Old(...)
 end)
+
+for i,v in next, getconnections(game:GetService("ScriptContext").Error) do 
+if v.Function then 
+hookfunction(v.Function, lol)
+print(v)
+end
+end
+
+for i,v in next, getconnections(game:GetService("LogService").MessageOut) do 
+if v.Function then 
+hookfunction(v.Function, lol)
+print(v)
+end
+end
